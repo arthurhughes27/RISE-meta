5883695b88f7a355b71d395df6656e2ad0f33228
Arthur Hughes <arthur.hughes27@outlook.com>
2026-03-23 17:39:45 +0100

    revised project structure

--- analysis/application/application_master.R
+++ analysis/application/application_master.R
@@ -1,3 +1,7 @@
 # Master script to run all application scripts
+# Main application script
 source("analysis/application/application_main.R")
-source("analysis/application/application_supplementary.R")
+
+# Supplementary scripts for sensitivity analysis
+source("analysis/application/application_supplementary_FE.R")
+source("analysis/application/application_supplementary_varEstim.R")
